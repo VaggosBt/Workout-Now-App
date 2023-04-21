2f83abc8b2a401c45ab17c9c74018c7d4691241d
Vaggos B <37471733+VaggosBt@users.noreply.github.com>
2023-04-21 19:36:53 +0300

    Create New Exercise Screen with Toolbar with custom theme and navigation arrow.

---
+++ app/src/main/java/com/udemycourses/workoutapp/ExerciseActivity.kt
@@ -0,0 +1,28 @@
+package com.udemycourses.workoutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.udemycourses.workoutapp.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+
+    private var binding: ActivityExerciseBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+
+        if(supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+
+    }
+}
--- app/src/main/java/com/udemycourses/workoutapp/MainActivity.kt
+++ app/src/main/java/com/udemycourses/workoutapp/MainActivity.kt
@@ -1,19 +1,37 @@
 package com.udemycourses.workoutapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.udemycourses.workoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding?  = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStartButton : FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener {
+        //val flStartButton : FrameLayout = findViewById(R.id.flStart)
+        /*flStartButton.setOnClickListener {
             Toast.makeText(this@MainActivity,"Here we start the application", Toast.LENGTH_SHORT).show()
+        }*/
+
+        binding?.flStart?.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
         }
 
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
+    }
+
 }
